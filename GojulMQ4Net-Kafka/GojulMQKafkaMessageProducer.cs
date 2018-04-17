6edda2745a1eb85d16d2582416246c483825bea5
Julien AUBIN <julien_aubin_lm@yahoo.fr>
2018-04-17 22:29:15 +0200

    added Kafka dependencies

--- GojulMQ4Net-Kafka/GojulMQKafkaMessageProducer.cs
+++ GojulMQ4Net-Kafka/GojulMQKafkaMessageProducer.cs
@@ -1,21 +1,23 @@
 ﻿using Org.Gojul.GojulMQ4Net_Api;
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Org.Gojul.GojulMQ4Net_Kafka
 {
-    public class GojulMQKafkaMessageProducer<T> : IGojulMQMessageConsumer<T>
+    public class GojulMQKafkaMessageProducer<T> : IGojulMQMessageProducer<T>
     {
-        public void ConsumeMessages(string topicName, IGojulMQMessageListener<T> messageListener)
-        {
-            throw new NotImplementedException();
-        }
-
         public void Dispose()
         {
             throw new NotImplementedException();
         }
 
-        public void StopConsumer()
+        public void SendMessage(string topic, IGojulMQMessageKeyProvider<T> messageKeyProvider, T message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessages(string topic, IGojulMQMessageKeyProvider<T> messageKeyProvider, IEnumerable<T> messages)
         {
             throw new NotImplementedException();
         }
