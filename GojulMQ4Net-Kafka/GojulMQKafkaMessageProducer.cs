e6cfdf17764900017bf32b4783a5fb46656867be
Julien AUBIN <julien_aubin_lm@yahoo.fr>
2018-04-17 20:34:00 +0200

    added original solution files

---
+++ GojulMQ4Net-Kafka/GojulMQKafkaMessageProducer.cs
@@ -0,0 +1,23 @@
+﻿using Org.Gojul.GojulMQ4Net_Api;
+using System;
+
+namespace Org.Gojul.GojulMQ4Net_Kafka
+{
+    public class GojulMQKafkaMessageProducer<T> : IGojulMQMessageConsumer<T>
+    {
+        public void ConsumeMessages(string topicName, IGojulMQMessageListener<T> messageListener)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopConsumer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
