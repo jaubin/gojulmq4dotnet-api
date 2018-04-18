9422863d2f67452e0f43adee83ed3a5666504c61
Julien AUBIN <julien_aubin_lm@yahoo.fr>
2018-04-18 19:19:57 +0200

    started implementation

--- GojulMQ4Net-Kafka/GojulMQKafkaMessageProducer.cs
+++ GojulMQ4Net-Kafka/GojulMQKafkaMessageProducer.cs
@@ -1,22 +1,82 @@
-﻿using Org.Gojul.GojulMQ4Net_Api;
+﻿using Common.Logging;
+using Conditions;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using Org.Gojul.GojulMQ4Net_Api;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Org.Gojul.GojulMQ4Net_Kafka
 {
+    /// <summary>
+    /// Class <code>GojulMQKafkaMessageProducer</code> is the Kafka implementation
+    /// of interface <code>IGojulMQMessageProducer</code>. Note that
+    /// this class is thread-safe, and that messages are automatically flushed
+    /// after being sent. Although this is not the fastest configuration, it is
+    /// the safest one for services intended to run as daemons like this one.
+    /// So in order to increase performance you should try to favor
+    /// batch sending whenever possible.
+    /// </summary>
+    /// <typeparam name="T">the type of messages to be produced. Note that this type must
+    /// have been generated following Avro conventions, as defined
+    /// <a href="https://dzone.com/articles/kafka-avro-serialization-and-the-schema-registry">there</a>.
+    /// </typeparam>
     public class GojulMQKafkaMessageProducer<T> : IGojulMQMessageProducer<T>
     {
+        /// <summary>
+        /// The bootstrap servers property used for configuration.
+        /// </summary>
+        public static readonly string BOOTSTRAP_SERVERS = "bootstrap.servers";
+
+        /// <summary>
+        /// The schema registry URL property used for configuration.
+        /// </summary>
+        public static readonly string SCHEMA_REGISTRY_URL = "schema.registry.url";
+
+        /// <summary>
+        /// The client ID property used for configuration.
+        /// </summary>
+        public static readonly string CLIENT_ID = "client.id";
+
+        private static readonly ILog log = LogManager.GetLogger<GojulMQKafkaMessageProducer<T>>();
+
+        private readonly Producer<string, T> producer;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="settings">the settings dictionary, which follows
+        /// the norm defined by Kafka settings.</param>
+        public GojulMQKafkaMessageProducer(Dictionary<string, object> settings)
+        {
+            Condition.Requires(settings, "settings").IsNotNull();
+            Condition.Requires((string) settings[BOOTSTRAP_SERVERS], BOOTSTRAP_SERVERS)
+                .IsNotNull()
+                .IsNotEmpty();
+            Condition.Requires((string)settings[CLIENT_ID], CLIENT_ID)
+                .IsNotNull()
+                .IsNotEmpty();
+            Condition.Requires((string)settings[SCHEMA_REGISTRY_URL], SCHEMA_REGISTRY_URL)
+                .IsNotNull()
+                .IsNotEmpty();
+
+            producer = new Producer<string, T>(settings, new StringSerializer(Encoding.UTF8), new AvroSerializer<T>());
+        }
+
+        /// <see cref="IDisposable.Dispose"/>
         public void Dispose()
         {
-            throw new NotImplementedException();
+            producer.Dispose();
         }
 
+        /// <see cref="IGojulMQMessageProducer{T}.SendMessage(string, IGojulMQMessageKeyProvider{T}, T)"/>
         public void SendMessage(string topic, IGojulMQMessageKeyProvider<T> messageKeyProvider, T message)
         {
-            throw new NotImplementedException();
+            SendMessages(topic, messageKeyProvider, new[] { message });
         }
 
+        /// <see cref="IGojulMQMessageProducer{T}.SendMessages(string, IGojulMQMessageKeyProvider{T}, IEnumerable{T})"/>
         public void SendMessages(string topic, IGojulMQMessageKeyProvider<T> messageKeyProvider, IEnumerable<T> messages)
         {
             throw new NotImplementedException();
