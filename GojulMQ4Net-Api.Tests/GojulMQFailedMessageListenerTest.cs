267dfffeece7bcbc18efcb606fa4a87a6403e9cf
Julien AUBIN <julien_aubin_lm@yahoo.fr>
2018-10-31 14:36:10 +0100

    Added a listener which forwards stuff to the hospital topic

---
+++ GojulMQ4Net-Api.Tests/GojulMQFailedMessageListenerTest.cs
@@ -0,0 +1,95 @@
+using System;
+
+using Moq;
+using Xunit;
+
+using Org.Gojul.GojulMQ4Net_Api;
+
+namespace Org.Gojul.GojulMQ4Net_Api.Tests
+{
+    public class GojulMQFailedMessageListenerTest: IDisposable
+    {
+        private bool consumed;
+
+        public GojulMQFailedMessageListenerTest()
+        {
+            consumed = false;
+        }
+
+        private void ListenMessage(object message)
+        {
+            consumed = true;
+        }
+
+        [Fact]
+        public void TestConstructorWithNullProducerThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GojulMQFailedMessageListener<object>(null, ListenMessage, "hello"));
+        }
+
+        [Fact]
+        public void TestConstructorWithNullListenerThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GojulMQFailedMessageListener<object>(new Mock<IGojulMQMessageProducer<object>>().Object, 
+                null, "hello"));
+        }
+
+        [Fact]
+        public void TestConstructorWithNullTopicThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GojulMQFailedMessageListener<object>(new Mock<IGojulMQMessageProducer<object>>().Object,
+                ListenMessage, null));
+        }
+
+        [Fact]
+        public void TestConstructorWithBlankTopicThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => new GojulMQFailedMessageListener<object>(new Mock<IGojulMQMessageProducer<object>>().Object,
+                ListenMessage, "   "));
+        }
+
+        [Fact]
+        public void TestOnMessageWithNullMessageThrowsException()
+        {
+            var listener = new GojulMQFailedMessageListener<object>(new Mock<IGojulMQMessageProducer<object>>().Object,
+                ListenMessage, "hello");
+            Assert.Throws<ArgumentNullException>(() => listener.OnMessage(null));
+        }
+
+        [Fact]
+        public void TestOnMessageWithMQExceptionForwardsException()
+        {
+            var listener = new GojulMQFailedMessageListener<object>(new Mock<IGojulMQMessageProducer<object>>().Object,
+                e => throw new GojulMQException(), "hello");
+
+            Assert.Throws<GojulMQException>(() => listener.OnMessage("world"));
+        }
+
+        [Fact]
+        public void TestOnMessageWithStandardExceptionThrowsMessageToErrorQueue()
+        {
+            Mock<IGojulMQMessageProducer<object>> producer = new Mock<IGojulMQMessageProducer<object>>();
+
+            var listener = new GojulMQFailedMessageListener<object>(producer.Object,
+                e => throw new Exception(), "hello");
+            listener.OnMessage("world");
+
+            producer.Verify(x => x.SendMessage("hello", It.IsAny<GojulMQMessageKeyProvider<object>>(), "world"));
+        }
+
+        [Fact]
+        public void TestOnMessage()
+        {
+            Mock<IGojulMQMessageProducer<object>> producer = new Mock<IGojulMQMessageProducer<object>>();
+
+            var listener = new GojulMQFailedMessageListener<object>(producer.Object, ListenMessage, "hello");
+            listener.OnMessage("world");
+
+            Assert.True(consumed);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ GojulMQ4Net-Api/GojulMQFailedMessageListener.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Serilog;
+using Conditions;
+
+namespace Org.Gojul.GojulMQ4Net_Api
+{
+    /// <summary>
+    /// Class <code>GojulMQFailedMessageListener</code> is a simple wrapper
+    /// around an usual message consumer listener which redirects failed
+    /// messages to an hospital topic.
+    /// </summary>
+    public class GojulMQFailedMessageListener<T>
+    {
+
+        private static readonly ILogger log = Serilog.Log.ForContext<GojulMQFailedMessageListener<T>>();
+
+        private readonly IGojulMQMessageProducer<T> producer;
+        private readonly GojulMQMessageListener<T> listener;
+        private readonly string errorTopic;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="producer">the producer in charge of redirecting erroneous objects.</param>
+        /// <param name="listener">the listener to decorate.</param>
+        /// <param name="errorTopic">the error topic used.</param>
+        /// <exception cref="ArgumentNullException">if any of the method parameters is <code>null</code>.</exception>
+        public GojulMQFailedMessageListener(IGojulMQMessageProducer<T> producer,
+            GojulMQMessageListener<T> listener, string errorTopic)
+        {
+            Condition.Requires(producer).IsNotNull();
+            Condition.Requires(listener).IsNotNull();
+            Condition.Requires(errorTopic).IsNotNullOrWhiteSpace();
+
+            this.producer = producer;
+            this.listener = listener;  
+            this.errorTopic = errorTopic;
+        }
+
+        /// <summary>
+        /// Process the message <code>message</code>.
+        /// </summary>
+        /// <param name="message">the message to process.</param>
+        /// <exception cref="ArgumentNullException">if <code>message</code> is null.</exception>
+        /// <exception cref="GojulMQException">if an error occurs while processing the message due to the message queue.</exception>
+        public void OnMessage(T message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException("message is null");
+            }
+
+            try
+            {
+                listener(message);
+            }
+            catch (GojulMQException e)
+            {
+                log.Error(e, "Error with the MQ system");
+                throw;
+            }
+            catch (Exception e)
+            {
+                log.Error(e, "Error processing message");
+                producer.SendMessage(errorTopic, msg => null, message);
+            }
+
+        }
+    }
+}
